ce05349eb5bc65e38c72deeab2bd2aa1eb59d438
Cody Arvonen <codyarvonen@null-8c85901ff858.app.byu.edu>
2021-04-10 14:48:07 -0600

    Updated Profile

--- components/ProfilePage.js
+++ components/ProfilePage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import data from './../db/student.json'
 import {
   StyleSheet,
   Text,
@@ -13,22 +14,27 @@ export default class Profile extends Component {
     return (
       <View style={styles.container}>
           <View style={styles.header}></View>
-          <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
+          <Image style={styles.avatar} source={{uri: "https://d2jyir0m79gs60.cloudfront.net/news/images/successful-college-student-lg.png"}}/>
           <View style={styles.body}>
             <View style={styles.bodyContent}>
-              <Text style={styles.name}>John Doe</Text>
               <Text style={styles.info}>UX Designer / Mobile developer</Text>
               <Text style={styles.description}>Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,</Text>
-              
-              <TouchableOpacity style={styles.buttonContainer}>
-                <Text>Opcion 1</Text>  
-              </TouchableOpacity>              
-              <TouchableOpacity style={styles.buttonContainer}>
-                <Text>Opcion 2</Text> 
-              </TouchableOpacity>
             </View>
+            <Text style={styles.name}> {data.name} </Text>
         </View>
-      </View>
+                <Text style={styles.info}> GPA: {data.gpa} </Text>
+                <Text style={styles.info}> School: {data.university} </Text>
+                <Text style={styles.info}> Major: {data.major} </Text>
+                <Text style={styles.info}> Degree Pursuing: {data.degree} </Text>
+                <Text style={styles.info}> Graduation Year: {data.grad_year} </Text>
+                <Text style={styles.info}> Relevant Work Experience:{"\n"} 1.) {data.relevant_exp[0]} {"\n"} 2.) {data.relevant_exp[1]} {"\n"} 3.) {data.relevant_exp[2]}</Text>
+                <Text style={styles.info}> Volunteer Experience:{"\n"} 1.) {data.volunteer_exp[0]} {"\n"} 2.) {data.volunteer_exp[1]} {"\n"} 3.) {data.volunteer_exp[2]}</Text>
+                <Text style={styles.info}> Awards:{"\n"} 1.) {data.awards[0]} {"\n"} 2.) {data.awards[1]} {"\n"} 3.) {data.awards[2]}</Text>
+                <Text style={styles.info}> Available to work: {data.availbility_date} </Text>
+                <Text style={styles.info}> LinkedIn Profile: {data.linked_in} </Text>
+
+        </View>
+        
     );
   }
 }
@@ -36,7 +42,13 @@ export default class Profile extends Component {
 const styles = StyleSheet.create({
   header:{
     backgroundColor: "#00BFFF",
-    height:200,
+    height:175,
+    borderTopLeftRadius: 10,    
+    borderTopRightRadius: 10,
+  },
+  container: {
+    backgroundColor: 'white',
+    borderRadius: 10,
   },
   avatar: {
     width: 130,
@@ -69,8 +81,8 @@ const styles = StyleSheet.create({
   },
   info:{
     fontSize:16,
-    color: "#00BFFF",
-    marginTop:10
+    color: "#696969",
+    marginTop:10,
   },
   description:{
     fontSize:16,
